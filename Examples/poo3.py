1e2379c095a286b578e2b2965040b64f0121fb51
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-10-30 00:11:37 -0300

    examples about inheritance

--- Examples/poo3.py
+++ Examples/poo3.py
@@ -11,8 +11,9 @@ class Persona:
     def modificarDomicilio(self, nuevodom):
         pass
 
-    def modificarTelefono(self, nuevotel): 
+    def modificarTelefono(self, nuevotel):
         pass
+
 
 class Empleado(Persona):
     sueldoBase = 70000
@@ -25,7 +26,18 @@ class Empleado(Persona):
         self.extraAntig = antig
 
     def calculaSueldo(self):
-        print('El sueldo de', self.nombre, 'es de $', self.sueldoBase + self.extraAntig + self.extraCargo)
+        print('El sueldo de', self.nombre, 'es de $',
+              self.sueldoBase + self.extraAntig + self.extraCargo)
 
-e1 = Empleado('Juan Pérez', '20/12/1968', 'Aráoz 234 - CABA', '20565412', '(011) 4555-5874','Ordenanza','L-452258',25000,65000)
+
+e1 = Empleado('Juan Pérez', '20/12/1968', 'Aráoz 234 - CABA', '20565412',
+              '(011) 4555-5874', 'Ordenanza', 'L-452258', 25000, 65000)
 e1.calculaSueldo()
+
+e2 = Empleado('Marta Perz', '01/01/2000', 'por ahi', '12345678',
+              '12345', 'ingeniaria', 'L-445566', 20000, 50000)
+e2.calculaSueldo()
+
+e3 = Empleado('Fulanito', '12/10/2000', 'en algun lugar', '12345',
+              '123456', 'marketing', 'L-22334', 30000, 60000)
+e3.calculaSueldo()
