dc836ad364f01eadb055916c032f957ecf22ce98
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-10-30 00:28:47 -0300

    examples about inheritance

--- Examples/poo3.py
+++ Examples/poo3.py
@@ -1,3 +1,6 @@
+from statistics import median
+
+
 class Persona:
 
     # MÉTODO DE INICIALIZACIÓN
@@ -29,6 +32,41 @@ class Empleado(Persona):
         print('El sueldo de', self.nombre, 'es de $',
               self.sueldoBase + self.extraAntig + self.extraCargo)
 
+
+class Paciente(Persona):
+    gastoBase = 1000
+
+    def __init__(self, nom, fn, dom, dni, tel, med):
+        super().__init__(nom, fn, dom, dni, tel)
+        self.especialidad = med
+
+    def calcularGasto(self):
+        if self.especialidad == 'odontologia':
+            self.gastoBase += 5000
+        elif self.especialidad == 'traumatologia':
+            self.gastoBase += 10000
+        elif self.especialidad == 'oftalmologia':
+            self.gastoBase += 7500
+        else:
+            self.gastoBase += 4000
+        return self.gastoBase
+
+    def gastoTotal(self):
+        print('el gasto total del paciente',
+              self.nombre, 'es: ', self.calcularGasto())
+
+
+paciente1 = Paciente("pedro", "02/04/1999", "ni idea",
+                     "444444", "147258369", 'odontologia')
+paciente1.gastoTotal()
+
+paciente2 = Paciente("juan", "02/02/1979", "ni idea",
+                     "444444", "3456789", 'psicologia')
+paciente2.gastoTotal()
+
+paciente3 = Paciente("marta", "07/05/2009", "ni idea",
+                     "445544", "3565164+", 'oftalmologia')
+paciente3.gastoTotal()
 
 e1 = Empleado('Juan Pérez', '20/12/1968', 'Aráoz 234 - CABA', '20565412',
               '(011) 4555-5874', 'Ordenanza', 'L-452258', 25000, 65000)
